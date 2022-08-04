2f71bb57047725ee2727e101bb7164dffc2de45e
woals990605 <woals990605@naver.com>
2022-08-04 19:24:15 +0900

    유튜브 5강중

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Sub from './Sub';
 
 function App() {
+
+  // let number = 1; // 상태 값 아님
+
+  const [number, setNumber] = useState(1); // React안에 hooks 라이브러리 상태값이 됨.
+
+  const add = () => {
+    setNumber(number + 1); // 리액트한테 number 값 변경할게 라고 요청
+    console.log("add", number);
+  }
+
+  // 랜더링 시점 = 상태값 변경
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>숫자 : {number}</h1>
+      <button onClick={add}>더하기</button>
+      <Sub />
     </div>
   );
 }
