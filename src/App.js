3db82928d72bc6f4b272ac92cc2eac5d506aedd9
woals990605 <woals990605@naver.com>
2022-08-08 11:18:41 +0900

    유튜브 9강까지

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
-import { useState } from 'react';
 import './App.css';
+import { createRef, useRef, useState } from "react";
 
-// useMemo => 메모라이제이션(기억)
 
+// useRef (디자인)
+// dom을 변경할 때 사용
 function App() {
+  const myRef = useRef(null)
 
-  const [list, setList] = useState([1, 2, 3, 4]);
+  const [list, setList] = useState([
+    { id: 1, name: "길동" },
+    { id: 2, name: "꺽정" }
+  ]);
 
-  const getAddResult = () => {
-    let sum = 0;
-    list.forEach(i => sum = sum + i);
-    console.log('sum 함수 실행됨:', sum);
-    return sum;
-  }
-
+  const myRefs = Array.from({ length: list.length }).map(() => createRef())
   return (
     <div>
-      <button onClick={() => { setList([...list, 10]); }}>리스트값 추가</button>
-      <div>
-        {list.map((i => <h1>{i}</h1>))}
-      </div>
-      <div>합계 : {getAddResult()}</div>
+      <button onClick={() => {
+        console.log(myRef)
+        console.log(myRef.current)
+        //myRef.current.style.backgroundColor = 'red';
+        myRefs[1].current.style.backgroundColor = 'red';
+      }}>색 변경
+      </button>
+      <div ref={myRef}>박스</div>
+      {list.map((user, index) => (
+        <h1 ref={myRefs[index]}>{user.name}</h1>
+      ))}
     </div>
   );
 }
 
 export default App;
+
